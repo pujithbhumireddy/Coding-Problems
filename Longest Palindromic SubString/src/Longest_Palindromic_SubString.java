0b9c95b6a8deaa5f4258311f112051f463252821
pujithbhumireddy <pujithbhumireddy@gmail.com>
2024-12-11 22:16:42 +0530

    updated

--- Longest Palindromic SubString/src/Longest_Palindromic_SubString.java
+++ Longest Palindromic SubString/src/Longest_Palindromic_SubString.java
@@ -1,49 +1,43 @@
 
 public class Longest_Palindromic_SubString {
-	
-	public static boolean isLongPalSs(String str)
-	{
-		
-		if(str == "" && str.length() == 0)
+
+	public static boolean isLongPalSs(String str) {
+
+		if (str == "" && str.length() == 0)
 			return false;
-		
-		int left =0;
-		int right = str.length()-1;
-		
-		while(left<right)
-		{
-			if(str.charAt(left) != str.charAt(right))
+
+		int left = 0;
+		int right = str.length() - 1;
+
+		while (left < right) {
+			if (str.charAt(left) != str.charAt(right))
 				return false;
-			
+
 			left++;
 			right--;
 		}
-		
+
 		return true;
 	}
-	
-	public static void main(String[] args)
-	{
-		String str = "aabadda";
-		
+
+	public static void main(String[] args) {
+		String str = "banana";
+
 		int len = str.length();
 		String longestSubString = "";
-		
-		for(int i=0;i<len;i++)
-		{
+
+		for (int i = 0; i < len; i++) {
 			String substr = "";
-			for(int j=i;j<len;j++)
-			{
-				substr += (str.charAt(j)+"");
-				
-				if(isLongPalSs(substr) && substr.length() > longestSubString.length())
-						longestSubString = substr;
+			for (int j = i; j < len; j++) {
+				substr += (str.charAt(j) + "");
+
+				if (isLongPalSs(substr) && substr.length() > longestSubString.length())
+					longestSubString = substr;
 			}
 		}
-		
-		System.out.println("the Longest Palindrome SubString :"+longestSubString);
-		
-		
+
+		System.out.println("the Longest Palindrome SubString :" + longestSubString);
+
 	}
-	
+
 }
